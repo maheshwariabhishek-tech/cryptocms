e7ad6e506198448b51b7663d841852897f32b705
Abhishek Maheshwari <abhishekmaheshwari01234@gmail.com>
2024-09-09 17:06:25 +0530

    update main

--- src/components/UI/card/Card.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import classes from "./Card.module.scss";
-const Card: React.FC = (props) => {
-  return <div className={classes.card}>{props.children}</div>;
-};
-
-export default Card;
--- src/pages/BlankPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function BlankPage() {
-  return <div>BlankPage</div>;
-}
-
-export default BlankPage;
